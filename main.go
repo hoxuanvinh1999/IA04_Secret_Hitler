a5192ddefc00a739fdea373f31edb892d87ded89
Nathan Le Boudec <nathan.le-boudec@etu.utc.fr>
2023-01-02 21:58:05 +0000

    New test

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
-/*func main() {
+func main() {
 	g := newGame([]string{"Vinh", "Wassim", "Pierre", "Sylvain", "Jérôme", "Nathan"})
+
+	// c := make(chan voteRequest)
+	// ponger := NewPongAgent("ponger", c)
+
+	// ponger.Start()
+	// name := []string{"Vinh", "Wassim", "Pierre", "Sylvain", "Nathan"}
+	// role := []string{Liberal, Fascist, Liberal, Hitler, Liberal}
+	// for i := 0; i < 5; i++ {
+	// 	//id := fmt.Sprintf("pinger n°%d", i)
+	// 	pinger := NewAgentPlayer(name[i], c, role[i], true, Liberal)
+	// 	pinger.Start()
+	// }
+	// time.Sleep(time.Second)
+
+	//g.start(ponger)
 	g.start()
 }
-*/
+
 //func (g *game) investigationAvailable() bool {
 //	return g.fascistPolicies >= 3
 //}
