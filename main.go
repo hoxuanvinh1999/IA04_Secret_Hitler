b7230a56dd343517c9100a3fc216e86d051c20e0
Nathan Le Boudec <nathan.le-boudec@etu.utc.fr>
2023-01-03 18:15:39 +0000

    Replace main.go

--- main.go
+++ main.go
@@ -1,10 +1,43 @@
 package main
 
-/*func main() {
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	c := make(chan voteRequest)
 	g := newGame([]string{"Vinh", "Wassim", "Pierre", "Sylvain", "Jérôme", "Nathan"})
+	g.c = c
+	// c := make(chan voteRequest)
+	// ponger := NewPongAgent("ponger", c)
+
+	// ponger.Start()
+	// name := []string{"Vinh", "Wassim", "Pierre", "Sylvain", "Nathan"}
+	// role := []string{Liberal, Fascist, Liberal, Hitler, Liberal}
+	// for i := 0; i < 5; i++ {
+	// 	//id := fmt.Sprintf("pinger n°%d", i)
+	// 	pinger := NewAgentPlayer(name[i], c, role[i], true, Liberal)
+	// 	pinger.Start()
+	// }
+	// time.Sleep(time.Second)
+
+	//g.start(ponger)
 	g.start()
+	c_to_agent := make(map[string]chan voteRequest)
+	//Création des agents joueurs
+	for _, p := range g.players {
+		newChan := make(chan voteRequest)
+		//c_to_agent = append(c_to_agent, newChan)
+		c_to_agent[p.name] = newChan
+		joueur := NewAgentPlayer(p.name, c, newChan, p.role, true, Liberal)
+		joueur.Start(g.players)
+	}
+	g.c_to_agent = c_to_agent
+	fmt.Print(g.c_to_agent)
+	time.Sleep(10 * time.Minute)
 }
-*/
+
 //func (g *game) investigationAvailable() bool {
 //	return g.fascistPolicies >= 3
 //}
