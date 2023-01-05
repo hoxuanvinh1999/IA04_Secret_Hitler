8c03797140f53f267b31f0dadfecf351323f07b3
Nathan Le Boudec <nathan.le-boudec@etu.utc.fr>
2023-01-05 00:55:27 +0000

    Replace main.go

--- main.go
+++ main.go
@@ -17,14 +17,16 @@ func main() {
 	c := make(chan voteRequest)
 	g := newGame([]string{"Vinh", "Wassim", "Pierre", "Sylvain", "Jérôme", "Nathan"})
 	g.c = c
+	g.result_game = "Jeu en cours"
 	g.end = false
 	g.start()
 	c_to_agent := make(map[string]chan voteRequest)
 	//Création des agents joueurs
+	menteur := 3.0
 	for _, p := range g.players {
 		newChan := make(chan voteRequest)
 		c_to_agent[p.name] = newChan
-		joueur := NewAgentPlayer(p.name, c, newChan, p.role, true, Liberal, g)
+		joueur := NewAgentPlayer(p.name, c, newChan, p.role, true, Liberal, g, menteur)
 		if joueur.role == Fascist || joueur.role == Hitler {
 			for i := 0; i < len(g.players); i++ {
 				if g.players[i].role == Fascist || g.players[i].role == Hitler {
